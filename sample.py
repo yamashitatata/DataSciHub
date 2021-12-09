e8916726fb65543432287f2cd2070cb7c97ab32f
unknown <24155454gm@gmail.com>
2021-12-09 12:44:09 +0900

    sample

---
+++ sample.py
@@ -0,0 +1,9 @@
+
+def sample(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+sample('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
